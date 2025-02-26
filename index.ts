ff558988d4358b1fd4f6c2f59fc39b953367cf03
OlehZ <Oleh_Zaslavskyy@epam.com>
2025-02-26 20:36:59 +0200

    Introduction practical task 1

---
+++ index.ts
@@ -0,0 +1,7 @@
+export default function getRandomNumber(): void {
+    const randomNumber = Math.floor(Math.random() * 1000) + 1;
+    console.log(`Random number: ${randomNumber}`);
+  }
+  
+  getRandomNumber();
+  
